9f533f2c0a4e4cb07e4a4c9ec9ee52e211cd3d72
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2023-03-16 17:01:31 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,21 +3,21 @@
 # Script source: https://medium.com/macoclock/automating-your-macos-setup-with-homebrew-and-cask-e2a103b51af1
 # Run script from github directly: sh -c "$(curl -fsSL https://raw.githubusercontent.com/tadeubanzato/homebrew/main/test.sh)"
 
-# Check for Xcode-Select
+## Check for Xcode-Select
 echo "Checking xcode-select!"
 if test ! $(which xcode-select); then
     echo "Installing xcode-select..."
     sudo xcode-select --install
 fi
 
-# Check for Homebrew to be present, install if it's missing
+## Check for Homebrew to be present, install if it's missing
 echo "Check Homebrew!"
 if test ! $(which brew); then
     echo "Installing homebrew..."
     sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# Check oh-my-zsh
+## Check oh-my-zsh
 echo "Check oh-my-zsh!"
 DIRECTORY="$HOME/.oh-my-zsh"
 if ! [ -d "$DIRECTORY" ]; then
@@ -35,27 +35,27 @@ if ! [ -d "$DIRECTORY" ]; then
 fi
 
 
-# # Update homebrew recipes
-# brew update
+## Update homebrew recipes
+brew update
 
-# ## Installs all primary casks
-# echo "Installing cask..."
-# CASKS=(
-#     hyper
-#     visual-studio-code
-#     microsoft-office
-#     slack
-#     spotify
-#     evernote
-#     webex
-#     dropbox
-#     notion
-#     github
-#     appcleaner
-#     google-chrome
-# )
-# echo "Installing cask apps..."
-# brew install --cask ${CASKS[@]}
+## Installs all primary casks
+echo "Installing cask..."
+CASKS=(
+    hyper
+    visual-studio-code
+    microsoft-office
+    slack
+    spotify
+    evernote
+    webex
+    dropbox
+    notion
+    github
+    appcleaner
+    google-chrome
+)
+echo "Installing cask apps..."
+brew install --cask ${CASKS[@]}
 
 ## Install all python packages using pip3
 echo "Installing lastest PIP3."
